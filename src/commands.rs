9b9a625b0a7bbc70b63eb8b14fb48e01def2a21b
bodqhrohro <bodqhrohro@yandex.ru>
2020-08-14 01:52:33 +0300

    Automatically pick a concise view of two amounts

--- src/commands.rs
+++ src/commands.rs
@@ -10,6 +10,7 @@ use std::path;
 use std::io;
 use std::io::{Read, Write};
 use std::str::FromStr;
+use std::collections::BTreeMap;
 
 use lazy_static::lazy_static;
 use linereader::LineReader;
@@ -23,6 +24,12 @@ pub struct Command
 }
 
 const FILE_PREFIX: &str = ".limon-";
+
+lazy_static! {
+    static ref DECIMAL_0: Decimal = Decimal::from(0);
+    static ref DECIMAL_1: Decimal = Decimal::from(1);
+    static ref DECIMAL_100: Decimal = Decimal::from(100);
+}
 
 // prefer /run, but /tmp is fine too
 lazy_static! {
@@ -88,6 +95,52 @@ fn cpu_freq_icon(cpu_no: &str) -> io::Result<String> {
     }
 }
 
+fn format_amount(mantissa: Decimal) -> String {
+    let is_round = mantissa.round_dp(3).fract() == *DECIMAL_0;
+
+    if !is_round && mantissa < *DECIMAL_1 {
+        format!("{:.2}", mantissa)
+    } else if !is_round && mantissa < *DECIMAL_100 {
+        format!("{:.1}", mantissa)
+    } else {
+        format!("{:.0}", mantissa)
+    }
+}
+
+lazy_static! {
+    static ref BYTE_SUFFIX_MAP: BTreeMap<u64, &'static str> = {
+        let mut map = BTreeMap::new();
+        map.insert(10 * (1 << 10), "K");
+        map.insert(10 * (1 << 20), "M");
+        map.insert(10 * (1 << 30), "G");
+        map.insert(10 * (1 << 40), "T");
+        map.insert(10 * (1 << 50), "P");
+        map
+    };
+}
+fn format_two_amounts(a1: u64, a2: u64, separator: &str) -> String {
+    let bearer = std::cmp::max(a1, a2);
+
+    let mut bearer_ceil = &10;
+    for ceil in BYTE_SUFFIX_MAP.keys() {
+        // stop when this ceil would lead to a leading zero or too many digits
+        if bearer < *ceil {
+            break;
+        }
+        bearer_ceil = ceil;
+    }
+
+    let bearer_suffix = match BYTE_SUFFIX_MAP.get(bearer_ceil) {
+        Some(suffix) => suffix,
+        None => "B"
+    };
+
+    let bearer_ceil = Decimal::from(bearer_ceil / 10);
+
+    format_amount(Decimal::from(a1) / bearer_ceil) + separator +
+        &format_amount(Decimal::from(a2) / bearer_ceil) + bearer_suffix
+}
+
 pub const LOADAVG:Command = Command {
     icon: '',
     call: |_| {
@@ -101,7 +154,6 @@ pub const LOADAVG:Command = Command {
 
 lazy_static! {
     static ref CPU_LINE_REGEXP: Regex = Regex::new(r"^cpu(\d+) ").unwrap();
-    static ref DECIMAL_100: Decimal = Decimal::new(100, 0);
 }
 pub const CPU:Command = Command {
     icon: '',
@@ -185,12 +237,138 @@ pub const MEM:Command = Command {
     call: |_| {
         match &*MEMINFO {
             Ok(meminfo) => {
-                let mem_available = meminfo.mem_available.unwrap_or(0) / 1024 / 1024;
-                let mem_total = meminfo.mem_total / 1024 / 1024;
+                let mem_available = meminfo.mem_available.unwrap_or(0);
+                let mem_total = meminfo.mem_total;
 
-                Some(format!("{}/{}M", mem_total - mem_available, mem_total))
+                Some(format_two_amounts(mem_total - mem_available, mem_total, "/"))
             },
             Err(_) => None,
         }
     },
 };
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // unexpected behaviour, but shoudn't fail at least
+    #[test]
+    fn amount_format_negative() {
+        let formatted = format_amount(Decimal::new(-2368, 2));
+        assert_eq!(formatted, "-23.68");
+    }
+
+    #[test]
+    fn amount_format_zero() {
+        let formatted = format_amount(Decimal::from(0));
+        assert_eq!(formatted, "0");
+    }
+
+    #[test]
+    fn amount_format_tiny() {
+        let formatted = format_amount(Decimal::new(1, 3));
+        assert_eq!(formatted, "0.00");
+    }
+
+    #[test]
+    fn amount_format_almost_one() {
+        let formatted = format_amount(Decimal::new(996, 3));
+        assert_eq!(formatted, "0.99");
+    }
+
+    #[test]
+    fn amount_format_one() {
+        let formatted = format_amount(Decimal::from(1));
+        assert_eq!(formatted, "1");
+    }
+
+    #[test]
+    fn amount_format_almost_ten() {
+        let formatted = format_amount(Decimal::new(975, 2));
+        assert_eq!(formatted, "9.7");
+    }
+
+    #[test]
+    fn amount_format_ten() {
+        let formatted = format_amount(Decimal::from(10));
+        assert_eq!(formatted, "10");
+    }
+
+    #[test]
+    fn amount_format_ten_plus() {
+        let formatted = format_amount(Decimal::new(1000001, 5));
+        assert_eq!(formatted, "10");
+    }
+
+    #[test]
+    fn amount_format_99() {
+        let formatted = format_amount(Decimal::new(9995, 2));
+        assert_eq!(formatted, "99.9");
+    }
+
+    #[test]
+    fn amount_format_hundred() {
+        let formatted = format_amount(Decimal::from(100));
+        assert_eq!(formatted, "100");
+    }
+
+    #[test]
+    fn amount_format_alot() {
+        let formatted = format_amount(Decimal::new(123456, 2));
+        assert_eq!(formatted, "1234");
+    }
+
+    #[test]
+    fn amount_format_huge() {
+        let formatted = format_amount(Decimal::new(123749089, 2));
+        assert_eq!(formatted, "1237490");
+    }
+
+    #[test]
+    fn two_amounts_bytes() {
+        let formatted = format_two_amounts(3, 687, "/");
+        assert_eq!(formatted, "3/687B");
+    }
+
+    #[test]
+    fn two_amounts_zero() {
+        let formatted = format_two_amounts(0, 0, ":");
+        assert_eq!(formatted, "0:0B");
+    }
+
+    #[test]
+    fn two_amounts_zero_of_more() {
+        let formatted = format_two_amounts(0, 102938, "'");
+        assert_eq!(formatted, "0'100K");
+    }
+
+    #[test]
+    fn two_amounts_mega() {
+        let formatted = format_two_amounts(1232899, 2389999, "=");
+        assert_eq!(formatted, "1204=2333K");
+    }
+
+    #[test]
+    fn two_amounts_mega_slight_asym() {
+        let formatted = format_two_amounts(1232899, 23899999, "⁚");
+        assert_eq!(formatted, "1.1⁚22.7M");
+    }
+
+    #[test]
+    fn two_amounts_mega_very_asym() {
+        let formatted = format_two_amounts(123289, 23899999, r"\");
+        assert_eq!(formatted, r"0.11\22.7M");
+    }
+
+    #[test]
+    fn two_amounts_mega_extreme_asym() {
+        let formatted = format_two_amounts(123289, 23899999999, "O");
+        assert_eq!(formatted, "0O22.2G");
+    }
+
+    #[test]
+    fn two_amounts_first_larger() {
+        let formatted = format_two_amounts(23899999999, 123289, "lol");
+        assert_eq!(formatted, "22.2lol0G");
+    }
+}
