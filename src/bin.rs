d9a8ad86c03f22d3fe2de5e11985c7719a4e6c9b
Bohdan Horbeshko <bodqhrohro@gmail.com>
2024-05-16 18:33:38 +0300

    Vivobook configuration

--- src/bin.rs
+++ src/bin.rs
@@ -14,24 +14,24 @@ pub fn main() {
     let args = env::args();
     let args = arguments::parse(args).unwrap();
 
-    let wireless_interface = ["wlan0"];
+    let wireless_interface = ["wlp2s0"];
 
     let cmds = vec![
         CommandAndArgs{command: &commands::Command::Static(commands::LOADAVG), args: &[]},
-        CommandAndArgs{command: &commands::Command::Static(commands::CPU), args: &[]},
+        // CommandAndArgs{command: &commands::Command::Static(commands::CPU), args: &[]},
         CommandAndArgs{command: &commands::Command::Static(commands::MEM), args: &[]},
         CommandAndArgs{command: &commands::Command::Static(commands::ZRAM), args: &[]},
-        CommandAndArgs{command: &commands::Command::Static(commands::RADEON_VRAM), args: &[]},
+        // CommandAndArgs{command: &commands::Command::Static(commands::RADEON_VRAM), args: &[]},
         CommandAndArgs{command: &commands::Command::Static(commands::TRAFFIC), args: &wireless_interface},
-        CommandAndArgs{command: &commands::Command::Static(commands::RADEON_TEMPERATURE), args: &[]},
+        // CommandAndArgs{command: &commands::Command::Static(commands::RADEON_TEMPERATURE), args: &[]},
         CommandAndArgs{command: &commands::Command::Static(commands::AMD_K10_TEMPERATURE), args: &[]},
-        CommandAndArgs{command: &commands::Command::Static(commands::ATA_HDDTEMP), args: &["/dev/sda"]},
-        CommandAndArgs{command: &commands::Command::Static(commands::ATA_GSENSE_ERROR_RATE), args: &["/dev/sda"]},
+        // CommandAndArgs{command: &commands::Command::Static(commands::ATA_HDDTEMP), args: &["/dev/sda"]},
+        // CommandAndArgs{command: &commands::Command::Static(commands::ATA_GSENSE_ERROR_RATE), args: &["/dev/sda"]},
         CommandAndArgs{command: &commands::Command::Static(commands::NETWORK_SPEED), args: &wireless_interface},
         CommandAndArgs{command: &commands::Command::Static(commands::WIRELESS_SIGNAL), args: &wireless_interface},
-        CommandAndArgs{command: &commands::Command::Static(commands::DISK_IO_SPEED), args: &["sda"]},
+        CommandAndArgs{command: &commands::Command::Static(commands::DISK_IO_SPEED), args: &["nvme0n1p1"]},
         CommandAndArgs{command: &commands::Command::Static(commands::FS_FREE), args: &["/"]},
-        CommandAndArgs{command: &commands::Command::Static(commands::UPS_VOLTAGE), args: &["nutdev"]},
+        // CommandAndArgs{command: &commands::Command::Static(commands::UPS_VOLTAGE), args: &["nutdev"]},
         CommandAndArgs{command: &commands::Command::Dynamic(commands::BATTERY), args: &[]},
     ];
 
